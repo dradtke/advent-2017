acbad281f18904c23d6341b21511d7f0dc46561c
Damien Radtke <damienradtke@gmail.com>
2017-12-22 14:11:02 -0600

    Day 9

---
+++ day09/part2.c
@@ -0,0 +1,62 @@
+#include <glib.h>
+#include <gio/gio.h>
+
+
+GInputStream *get_input(const gchar *name)
+{
+	GError *err = NULL;
+	GFile *file = g_file_new_for_path(name);
+	GFileInputStream *stream = g_file_read(file, NULL, &err);
+	if (err != NULL) {
+		g_printerr("unable to read file: %s\n", err->message);
+		return NULL;
+	}
+	return G_INPUT_STREAM(stream);
+}
+
+gint main(void)
+{
+	GInputStream *stream = get_input("input.txt");
+	if (stream == NULL) {
+		return 1;
+	}
+
+	gchar *buffer = g_malloc(2);
+	buffer[1] = '\0';
+	GError *error = NULL;
+
+	gint garbage_removed = 0;
+	gboolean skip_next = FALSE, is_junk = FALSE;
+	while (g_input_stream_read(stream, buffer, 1, NULL, &error) && !error) {
+		if (skip_next) {
+			skip_next = FALSE;
+			continue;
+		}
+
+		if (buffer[0] == '!') {
+			skip_next = TRUE;
+			continue;
+		}
+
+		if (is_junk) {
+			if (buffer[0] == '>')
+				is_junk = FALSE;
+			else
+				garbage_removed++;
+
+			continue;
+		} else {
+			if (buffer[0] == '<') {
+				is_junk = TRUE;
+				continue;
+			}
+		}
+	}
+
+	if (error) {
+		g_printerr("error: %s\n", error->message);
+		return 1;
+	}
+
+	g_print("%d\n", garbage_removed);
+}
